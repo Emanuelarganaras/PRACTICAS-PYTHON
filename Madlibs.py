a5e0959017d7f9ff5fc21d3c462c0c10911ea0c0
EArganaras <130569887+Emanuelarganaras@users.noreply.github.com>
2025-03-12 13:23:52 -0300

    Add files via upload
    Sencillo juego de "historias locas o madlibs"

---
+++ Madlibs.py
@@ -0,0 +1,11 @@
+
+adj= input("Adjetivo: ")
+verbo1= input("Verbo: ")
+verbo2= input("Verbo: ")
+sustantivo_plural= input("Sustantivo: ")
+
+
+
+madlib= f"Programar es tan {adj}. Siempre me emociona porque me encanta {verbo1} problemas. Aprende a {verbo2} con freeCodeCamp y alcanza tus {sustantivo_plural}!"
+
+print(madlib)
